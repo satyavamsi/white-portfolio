21d4a438ebaf2451dcaf0e40f846ba0225147b13
satyavamsi <satyavamsi222.21@gmail.com>
2018-11-18 17:48:29 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { ArticlesComponent } from './main/articles/articles.component';
+import { UserComponent } from './main/user/user.component';
+import { EducationComponent } from './main/education/education.component';
+import { ExperienceComponent } from './main/experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ArticlesComponent,
+    UserComponent,
+    EducationComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
